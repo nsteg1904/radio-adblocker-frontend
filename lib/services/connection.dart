61981aa6764d205aec72ae3c6504a684ab8483a6
CofingToffee <nh1236s@ad.fh-aachen.de>
2023-11-27 12:15:17 +0100

    -Fehlerhaften Code entfernt (apiConnection.dart)

--- lib/services/connection.dart
+++ lib/services/connection.dart
@@ -1,44 +1,44 @@
-import 'dart:async';
-import 'dart:convert';
-
-import 'dart:io';
-import 'package:web_socket_channel/io.dart';
-
-import '../model/radioStation.dart';
-
-class Conncection {
-  static Conncection? instance;
-  IOWebSocketChannel? channel;
-
-  Future<void> _initWebSocket() async {
-    try {
-      await WebSocket.connect('ws://185.233.107.253:5000/api')
-          .timeout(Duration(seconds: 20))
-          .then((ws) {
-            channel = IOWebSocketChannel(ws);
-      });
-    } catch (e) {
-      if (e is TimeoutException) {
-        print('Error: Connection timed out');
-      } else {
-        print('Error: Connection failed. ${e.toString()}');
-      }
-    }
-    return null;
-  }
-
-  IOWebSocketChannel? getChannel() {
-    return channel;
-  }
-
-  static Conncection? getInstance() {
-    if (instance == null) {
-      instance = new Conncection();
-      instance!._initWebSocket();
-    }
-    return instance;
-  }
-}
+// import 'dart:async';
+// import 'dart:convert';
+//
+// import 'dart:io';
+// import 'package:web_socket_channel/io.dart';
+//
+// import '../model/radioStation.dart';
+//
+// class Conncection {
+//   static Conncection? instance;
+//   IOWebSocketChannel? channel;
+//
+//   Future<void> _initWebSocket() async {
+//     try {
+//       await WebSocket.connect('ws://185.233.107.253:5000/api')
+//           .timeout(Duration(seconds: 20))
+//           .then((ws) {
+//             channel = IOWebSocketChannel(ws);
+//       });
+//     } catch (e) {
+//       if (e is TimeoutException) {
+//         print('Error: Connection timed out');
+//       } else {
+//         print('Error: Connection failed. ${e.toString()}');
+//       }
+//     }
+//     return null;
+//   }
+//
+//   IOWebSocketChannel? getChannel() {
+//     return channel;
+//   }
+//
+//   static Conncection? getInstance() {
+//     if (instance == null) {
+//       instance = new Conncection();
+//       instance!._initWebSocket();
+//     }
+//     return instance;
+//   }
+// }
 
 // class Connection {
 //   static Connection? _instance;
@@ -83,6 +83,53 @@ class Conncection {
 //     return _instance!;
 //   }
 // }
+
+// import 'dart:async';
+// import 'dart:io';
+// import 'package:web_socket_channel/io.dart';
+//
+// class Connection {
+//   static Connection? _instance;
+//   IOWebSocketChannel? _mainChannel;
+//   Map<String, IOWebSocketChannel> _channels = {};
+//
+//   Connection._(); // Privater Konstruktor
+//
+//   static Future<Connection> getInstance() async {
+//     if (_instance == null) {
+//       _instance = Connection._();
+//       await _instance!._initWebSocket();
+//     }
+//     return _instance!;
+//   }
+//
+//   Future<void> _initWebSocket() async {
+//     try {
+//       final WebSocket ws = await WebSocket.connect('ws://185.233.107.253:5000/api')
+//           .timeout(Duration(seconds: 20));
+//
+//       _mainChannel = IOWebSocketChannel(ws);
+//     } catch (e) {
+//       if (e is TimeoutException) {
+//         print('Error: Connection timed out');
+//       } else {
+//         print('Error: Connection failed. ${e.toString()}');
+//       }
+//     }
+//   }
+//
+//   IOWebSocketChannel getChannel(String channelId) {
+//     if (_mainChannel == null) {
+//       throw Exception('Main channel not initialized. Call getInstance first.');
+//     }
+//
+//     if (!_channels.containsKey(channelId)) {
+//       _channels[channelId] = IOWebSocketChannel(_mainChannel!.streamController);
+//     }
+//     return _channels[channelId]!;
+//   }
+// }
+
 
 
 
