3181a41cfaa3b408faa7c23cb87c6567054c92fc
Fadi Aljabri <fadialjabri1@gmail.com>
2024-02-19 13:42:34 +0100

    Light/Darkmode persistiert - Dart Docs hinzugefügt

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// This class is responsible for managing the theme of the app.
 class ThemeProvider with ChangeNotifier {
   bool _isDarkMode = true;
 
@@ -9,13 +10,13 @@ class ThemeProvider with ChangeNotifier {
   ThemeProvider() {
     _loadTheme();
   }
-
+/// Loads the theme from the shared preferences.
   void _loadTheme() async {
     final prefs = await SharedPreferences.getInstance();
     _isDarkMode = (prefs.getBool('isDarkMode') ?? true);
     notifyListeners();
   }
-
+/// Toggles the theme of the app.
   void toggleTheme() async {
     final prefs = await SharedPreferences.getInstance();
     _isDarkMode = !_isDarkMode;
